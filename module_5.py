2bccbdc3a8a56573c88cb1c445ac9eb1cdadaca1
FalleN <vovan095@gmail.com>
2021-05-08 02:45:44 +0300

    to do: "Добавить проверку на выход чела за пределы виджета"

---
+++ module_5.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- config: utf-8 -*-
+
+from tkinter import *
+from random import random
+
+
+def on_click():
+    x = random()
+    y = random()
+    bt1.place(relx=x, rely=y)
+
+
+root = Tk()
+root['bg'] = 'white'
+root.title('ЧЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЛ')
+img = PhotoImage(file='chel.png')
+
+bt1 = Button(image=img, command=on_click)
+bt1.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+root.mainloop()
